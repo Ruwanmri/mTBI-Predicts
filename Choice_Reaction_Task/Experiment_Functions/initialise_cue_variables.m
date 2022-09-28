bc4091b034fad496ca94ccfd148cdf57f7ad48ce
Ruwan_Wanni <103044650+Ruwan88@users.noreply.github.com>
2022-09-28 09:00:39 +0100

    Delete Choice_Reaction_Task directory

--- Choice_Reaction_Task/Experiment_Functions/initialise_cue_variables.m
+++
@@ -1,9 +0,0 @@
-function cfgCue = initialise_cue_variables
-% cfgCue = initialise_stim_variables
-% initialise variables of cue and grating
-
-cfgCue.destRectCueSize = 3;  % size of destination rectangle for cue
-cfgCue.cueToB = [0, 2, 0, 2];  % how many visual degrees from centre is cue presented (below)
-
-end
-
--- Choice_Reaction_Task/Experiment_Functions/make_texture_images.m
+++
@@ -1,9 +0,0 @@
-function presentingStr = make_texture_images(cfgScreen, cfgCue, cfgExp)  
-% presentingStr = make_texture_images(cfgScreen, cfgCue, cfgExp)   
-% makes textures of visual stim and cue
-
-for stim = cfgExp.numStim:-1:1  % create an openGL texture for cue images
-    presentingStr.cueStim{stim} = Screen('MakeTexture', cfgScreen.window, cfgCue.cueStim{stim});
-end
-
-end
