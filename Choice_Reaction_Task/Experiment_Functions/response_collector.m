d5b2ba74f835382c8deea0093634dd12290134e4
Ruwan_Wanni <103044650+Ruwan88@users.noreply.github.com>
2022-09-28 09:54:34 +0100

    CRT for MRI(Inginia-SPMIC)

---
+++ Choice_Reaction_Task/Experiment_Functions/response_collector.m
@@ -0,0 +1,46 @@
+function cfgOutput = response_collector(cfgExp, cfgOutput, cfgTrigger, nstim, cfgTxt, cfgScreen, cfgFile, cfgEyelink, cfgCue)
+% cfgOutput = response_collector(cfgExp, cfgOutput, cfgTrigger, nstim, cfgTxt, cfgScreen, cfgFile, cfgEyelink, cfgCue)
+% listens for participant's response
+
+noResp = 1;
+while noResp
+    [presd, firstPrsd] = KbQueueCheck(cfgExp.deviceNum);  % listens for response
+    keyCod = find(firstPrsd, 1);  % collects the pressed key code
+    
+    if presd && ismember(keyCod, cfgExp.responses) % store response variables
+        if ismember(keyCod, [cfgExp.NATAKeyR, cfgExp.respKeyR])
+            cfgOutput.respTmPnt(nstim) = send_trigger(cfgTrigger, cfgExp, cfgTrigger.respRight, cfgEyelink, 'right button press');  % send the resp trigger
+        elseif ismember(keyCod, [cfgExp.NATAKeyL, cfgExp.respKeyL])
+            cfgOutput.respTmPnt(nstim) = send_trigger(cfgTrigger, cfgExp, cfgTrigger.respLeft, cfgEyelink, 'left button press');  % send the resp trigger
+        end
+        cfgOutput.respTmKbQueue(nstim) = firstPrsd(keyCod);  % exact time of button press - more useful
+        cfgOutput.keyName{nstim} = KbName(keyCod);  % which key was pressed
+        cfgOutput.RT_KbQueue(nstim) = cfgOutput.respTmKbQueue(nstim) - cfgOutput.cueOnTmPnt(nstim);  % calculates RT - using time point in KbQueue
+        cfgOutput.RT_trig(nstim) = cfgOutput.respTmPnt(nstim) - cfgOutput.cueOnTmPnt(nstim);  % calculates RT - using triggers
+        
+        KbQueueFlush;
+        noResp = 0;
+        break
+    elseif presd && keyCod == cfgExp.quitKey
+        Screen('Flip', cfgScreen.window);
+        DrawFormattedText(cfgScreen.window, cfgTxt.quitTxt, 'center', 'center', [cfgScreen.white, cfgScreen.white, cfgScreen.white]);
+        Screen('Flip', cfgScreen.window);
+        [~, abrtPrsd] = KbStrokeWait;
+        if abrtPrsd(cfgExp.yesKey)
+            cfgOutput.abrtTmPoint = send_trigger(cfgTrigger, cfgExp, cfgTrigger.abort, cfgEyelink, 'Experiment aborted by operator');  % send the quit trigger
+            cfgOutput = cleanup(cfgFile, cfgExp, cfgScreen, cfgEyelink, cfgOutput, cfgTrigger, cfgTxt, cfgCue);
+            warning('Experiment aborted by operator')
+            break
+        end
+        KbQueueFlush;
+    end
+    
+    if (GetSecs - cfgOutput.cueOnTmPnt(nstim)) > ms2sec(cfgExp.respTimOut)  % stop listening after 1500msec
+        KbQueueFlush;
+        noResp = 0;
+        break
+    end
+end
+end
+
+
